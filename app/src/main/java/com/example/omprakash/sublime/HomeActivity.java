59e9ca86b5f65cae0024cbebbe5470a0bc1dd16f
opyadav121 <39383054+opyadav121@users.noreply.github.com>
2019-06-03 11:33:41 +0530

    123

--- app/src/main/java/com/example/omprakash/sublime/HomeActivity.java
+++ app/src/main/java/com/example/omprakash/sublime/HomeActivity.java
@@ -1,23 +1,46 @@
 package com.example.omprakash.sublime;
 
+import android.content.Context;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import Fragments.BankFragment;
 import Fragments.HomeFragment;
 import Fragments.ProfileFragment;
 import Fragments.ShopFragment;
+import me.relex.circleindicator.CircleIndicator;
 
 public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private ActionBar toolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private static ViewPager mPager;
+    private static int currentPager = 0;
+    ArrayList<Integer> picArray=new ArrayList<Integer>();
+    private static final Integer [] pic = {R.drawable.a, R.drawable.b,R.drawable.c,
+            R.drawable.e,R.drawable.f,R.drawable.d,R.drawable.g};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +50,38 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
         Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(false);
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayShowTitleEnabled(true);
+        //actionBar.setTitle(" Sublime ");
         actionBar.setIcon(R.drawable.ic_menu_black_24dp);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
-       // getSupportActionBar().setIcon(R.drawable.header1);
         actionBar.show();
 
-       // loadFragment(new HomeFragment());
 
+     /*   mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                toolbar,
+                R.string.drawer_Open,
+                R.string.drawer_Close
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle("LetsCatch");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle("LetsCatch");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };    */
+
+
+       // loadFragment(new HomeFragment());
+        Slider1();
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) this);
     }
@@ -80,6 +125,80 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
         }
         return false;
     }
+
+    public void Slider1(){
+        for (int i=0; i<pic.length; i++)
+            picArray.add(pic[i]);
+        mPager=findViewById(R.id.pager);
+        mPager.setAdapter(new MyAdapter(picArray,this));
+
+
+        CircleIndicator indicator=findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+        final Handler handler=new Handler();
+        final Runnable update=new Runnable() {
+            @Override
+            public void run() {
+                if (currentPager==pic.length)
+                {
+                    currentPager=0;
+                }
+                mPager.setCurrentItem(currentPager++,true);
+            }
+        };
+        Timer swipeTime=new Timer();
+        swipeTime.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        },1000,1000);
+    }
+    public class MyAdapter extends PagerAdapter
+    {
+        private ArrayList<Integer> images;
+        private LayoutInflater inflater;
+        private Context context;
+
+        public MyAdapter(ArrayList<Integer> images, Context context) {
+            this.images = images;
+            inflater = LayoutInflater.from(context);
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return images.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object)
+        {
+
+            return view.equals(object);
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object)
+        {
+            container.removeView((View) object);
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position)
+        {
+            View v=inflater.inflate(R.layout.slide,container,false);
+
+            ImageView myImage=v.findViewById(R.id.image);
+            myImage.setImageResource(images.get(position));
+            container.addView(v,0);
+            return v;
+        }
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
